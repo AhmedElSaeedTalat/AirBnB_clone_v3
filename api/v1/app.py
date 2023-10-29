be04250be5344136772779ff4375c1044434cb27
wanjanyamu01@gmail.com <liznyaporo@gmail.com>
2023-10-29 13:07:53 +0300

    api

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,6 +4,9 @@ from models import storage
 from api.v1.views.index import app_views
 from os import environ
 from flask import Flask, make_response, jsonify
+from flask_cors import CORS
+from flasgger import Swagger
+from flasgger.utils import swag_from
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
