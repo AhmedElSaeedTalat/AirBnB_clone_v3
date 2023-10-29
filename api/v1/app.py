9fdd99cbfc9588b9802b00fbe061b4f16b031ddb
wanjanyamu01@gmail.com <liznyaporo@gmail.com>
2023-10-29 13:10:07 +0300

    api

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,9 +4,6 @@ from models import storage
 from api.v1.views.index import app_views
 from os import environ
 from flask import Flask, make_response, jsonify
-from flask_cors import CORS
-from flasgger import Swagger
-from flasgger.utils import swag_from
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
@@ -28,14 +25,6 @@ def not_found(error):
         description: a resource was not found
     """
     return make_response(jsonify({'error': "Not found"}), 404)
-
-
-app.config['SWAGGER'] = {
-    'title': 'AirBnB clone Restful API',
-    'uiversion': 3
-}
-
-Swagger(app)
 
 
 if __name__ == "__main__":
